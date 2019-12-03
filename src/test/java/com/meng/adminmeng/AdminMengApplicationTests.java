32aafbbb91d1f48807330063f4ccb563ad0749eb
mengye <mengyexz@126.com>
2019-12-03 14:43:39 +0800

    intit

---
+++ src/test/java/com/meng/adminmeng/AdminMengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.adminmeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminMengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
