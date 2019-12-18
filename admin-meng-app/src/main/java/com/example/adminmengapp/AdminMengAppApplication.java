6537ed07a6d3d3b8f1dc38620130206e48a8b381
HAPPY-MENG\mengye <mengyexz@126.com>
2019-12-18 23:19:54 +0800

    提交文件

--- admin-meng-app/src/main/java/com/example/adminmengapp/AdminMengAppApplication.java
+++ admin-meng-app/src/main/java/com/example/adminmengapp/AdminMengAppApplication.java
@@ -1,7 +1,5 @@
 package com.example.adminmengapp;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdminMengAppApplication {
--- admin-meng-core/src/test/java/com/example/adminmengcore/AdminMengCoreApplicationTests.java
+++ admin-meng-core/src/test/java/com/example/adminmengcore/AdminMengCoreApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.adminmengcore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AdminMengCoreApplicationTests {
 
-    @Test
+    /*@Test
     void contextLoads() {
-    }
+    }*/
 
 }
