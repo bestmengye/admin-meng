dc53f4963db300d0778cc50205971ca5fa028302
mengye <mengyexz@126.com>
2019-12-19 15:00:10 +0800

    整理依赖和版本名称

--- admin-meng-app/src/main/java/com/example/adminmengapp/AdminMengAppApplication.java
+++ admin-meng-app/src/main/java/com/example/adminmengapp/AdminMengAppApplication.java
@@ -1,6 +1,9 @@
 package com.example.adminmengapp;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class AdminMengAppApplication {
 
